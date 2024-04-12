41a8b20fc1c9b3d9b1f667a851e5590b54089a59
Romain THOMAS <38260500+romain894@users.noreply.github.com>
2024-04-12 20:32:23 +0200

    paragraphs extraction ok

--- pdf_extraction/generate_documents.py
+++ pdf_extraction/generate_documents.py
@@ -2,7 +2,18 @@ from bs4 import BeautifulSoup
 
 test_file = "../data/tei_xml_collection/1-s2.0-S0025326X17306501-main.grobid.tei.xml"
 
-with open(test_file, 'r') as tei:
-    soup = BeautifulSoup(tei, 'lxml-xml')
 
-print(soup.title.getText())
+def get_paragraphs_from_xml(xml_file_path):
+    with open(xml_file_path, 'r') as tei:
+        soup = BeautifulSoup(tei, 'lxml-xml')
+
+    full_text = soup.body.find_all('p')
+    paragraphs = [None] * len(full_text)
+
+    for i, paragraph in enumerate(full_text):
+        paragraphs[i] = paragraph.text
+
+    return paragraphs
+
+
+print(get_paragraphs_from_xml(test_file))
