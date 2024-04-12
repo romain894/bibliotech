63e7d803600c27e8167fff5a70feb4c3b3c24420
Romain THOMAS <38260500+romain894@users.noreply.github.com>
2024-04-12 19:47:42 +0200

    grobid + python script move files ok

---
+++ data/tei_xml_collection/README.md
@@ -0,0 +1,1 @@
+# Folder for the processed TEI XML files
--- pdf_extraction/pdf_extraction.py
+++ pdf_extraction/pdf_extraction.py
@@ -1,5 +1,20 @@
+import json
 from grobid_client.grobid_client import GrobidClient
+import os, re, shutil
 from bs4 import BeautifulSoup
 
+# load config
+with open("config.json", "r") as f:
+    config = json.load(f)
+
 client = GrobidClient(config_path="./grobid_config.json")
-client.process("processFulltextDocument", "../data/pdf_new", n=10)
+
+# process all the PDF in the directory data/pdf_new
+client.process("processFulltextDocument", config['pdf_new_path'], n=10)
+
+# move the processed files
+for filename in os.listdir(config['pdf_new_path']):
+    # if re.match(r'.*\.tei\.xml$', filename):
+    if ".grobid.tei.xml" == filename[-15:]:
+        shutil.move(os.path.join(config['pdf_new_path'], filename), config['pdf_collection_path'])
+        shutil.move(os.path.join(config['pdf_new_path'], filename[:-15]+".pdf"), config['tei_xml_collection_path'])
