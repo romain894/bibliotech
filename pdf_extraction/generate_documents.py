fa23171c56535f5d314025b115ac2b6726bc89e8
Romain THOMAS <38260500+romain894@users.noreply.github.com>
2024-04-12 20:01:29 +0200

    first parsed title

--- pdf_extraction/generate_documents.py
+++ pdf_extraction/generate_documents.py
@@ -1,2 +1,8 @@
 from bs4 import BeautifulSoup
 
+test_file = "../data/tei_xml_collection/1-s2.0-S0025326X17306501-main.grobid.tei.xml"
+
+with open(test_file, 'r') as tei:
+    soup = BeautifulSoup(tei, 'lxml-xml')
+
+print(soup.title.getText())
