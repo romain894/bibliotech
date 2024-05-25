d79059e8de5648028c96412c8878a4156a13a0ef
Romain THOMAS <38260500+romain894@users.noreply.github.com>
2024-05-26 01:03:24 +0200

    upgraded extract_pdf.py to work in a deamon

--- extract_pdf.py
+++ extract_pdf.py
@@ -1,26 +1,76 @@
 import os
 import shutil
+import logging
 
 from dotenv import load_dotenv
 from grobid_client.grobid_client import GrobidClient
+
+from manipulate_documents import get_articles_list
 
 # load environment variables from .env file
 load_dotenv()
 
 pdf_new_path = os.getenv('PDF_NEW_PATH')
+pdf_ingestion_failed_path = os.getenv('PDF_INGESTION_FAILED')
 pdf_collection_path = os.getenv('PDF_COLLECTION_PATH')
 tei_xml_collection_path = os.getenv('TEI_XML_COLLECTION_PATH')
-documents_path = os.getenv('DOCUMENTS_PATH')
+
+pdf_being_extracted_path = os.path.join(pdf_new_path, "pdf_being_extracted")
+os.makedirs(pdf_being_extracted_path, exist_ok=True)
+pdf_ingestion_failed_already_exists_path = os.path.join(pdf_ingestion_failed_path, "pdf_already_ingested")
+os.makedirs(pdf_ingestion_failed_already_exists_path, exist_ok=True)
+pdf_ingestion_failed_grobid_extraction_failed_path = os.path.join(pdf_ingestion_failed_path, "grobid_extraction_failed")
+os.makedirs(pdf_ingestion_failed_grobid_extraction_failed_path, exist_ok=True)
 
 client = GrobidClient(config_path="grobid_config.json")
 
-# process all the PDF in the directory data/pdf_new
-client.process("processFulltextDocument", pdf_new_path, n=os.cpu_count())
 
-# move the processed files
-for filename in os.listdir(pdf_new_path):
-    if ".grobid.tei.xml" == filename[-15:]:
-        shutil.move(os.path.join(pdf_new_path, filename), tei_xml_collection_path)
-        shutil.move(os.path.join(pdf_new_path, filename[:-15]+".pdf"), pdf_collection_path)
+def extract_pdf():
+    # check if the new file names are not already in the collection
+    documents_in_collection = get_articles_list()
+    for filename in os.listdir(pdf_new_path):
+        if filename in documents_in_collection:
+            logging.error("A PDF file in the collection already exists with the same name: " + filename)
+            shutil.move(
+                os.path.join(pdf_new_path, filename),
+                os.path.join(pdf_ingestion_failed_path, "pdf_already_ingested")
+            )
 
-print("Done.")
+    nb_pdf = 0
+    # move pdf to be extracted in a dedicated directory
+    for filename in os.listdir(pdf_new_path):
+        if filename.lower().endswith(".pdf") and filename[-4:] != ".pdf":
+            os.rename(os.path.join(pdf_new_path, filename), os.path.join(pdf_new_path, filename[-4:] + ".pdf"))
+        if filename.endswith(".pdf"):
+            nb_pdf += 1
+            shutil.move(os.path.join(pdf_new_path, filename), pdf_being_extracted_path)
+
+    # no PDF to extract
+    if nb_pdf == 0:
+        return
+
+    # process all the PDF in the directory data/pdf_new
+    client.process("processFulltextDocument", pdf_being_extracted_path, n=int((os.cpu_count() + 1) / 2))
+
+    nb_pdf_success = 0
+    # move the processed files
+    for filename in os.listdir(pdf_being_extracted_path):
+        if ".grobid.tei.xml" == filename[-15:]:
+            nb_pdf_success += 1
+            shutil.move(os.path.join(pdf_being_extracted_path, filename), tei_xml_collection_path)
+            shutil.move(os.path.join(pdf_being_extracted_path, filename[:-15] + ".pdf"), pdf_collection_path)
+
+    nb_pdf_failed = 0
+    # for all remaining pdf in pdf_being_extracted_path, aka pdf which experienced an error during the extraction
+    for filename in os.listdir(pdf_being_extracted_path):
+        nb_pdf_failed += 1
+        logging.error("The following PDF file experienced an error during the extraction with GROBID: " + filename)
+        shutil.move(os.path.join(pdf_being_extracted_path, filename),
+                    pdf_ingestion_failed_grobid_extraction_failed_path)
+
+    logging.info(
+        f"The extraction of {nb_pdf} PDFs finished with {nb_pdf_success} successes and {nb_pdf_failed} failures.")
+
+
+if __name__ == '__main__':
+    extract_pdf()
